8d48d24177a9283cc3a12ed2265e4dfe38972082
balqeesHmoud <balqeeshmoud2@gmail.com>
2024-07-28 21:29:28 +0300

    lab28

--- cars_store/settings.py
+++ cars_store/settings.py
@@ -31,14 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'cars_API',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'cars_API',
+    
+   
 ]
 
 MIDDLEWARE = [
